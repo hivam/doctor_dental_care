d5bebf60c62c2dcee9edfa175a088fbb9d5e1044
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-01-24 12:28:40 -0500

    agregando cambio para que cargue la historia de acuerdo al modelo que la cita esta relacionado

--- models/doctor_appointment_inherit.py
+++ models/doctor_appointment_inherit.py
@@ -40,17 +40,17 @@ class doctor_appointment(osv.osv):
 			self.write(cr, uid, doctor_appointment.id, {'state': 'attending'}, context=context)
 		self.write(cr, uid, doctor_appointment.id, {'attended': True}, context=context)
 		# Get appoinment type
-		appointment_type = doctor_appointment.type_id.name
+		tipo_historia = doctor_appointment.type_id.modulos_id.name
 
 		profesional_id = doctor_appointment.schedule_id.professional_id.id
 
 		#GET model of the viewpg 
 		data_obj = self.pool.get('ir.model.data')
 
-		_logger.info(appointment_type)		
+		_logger.info(tipo_historia)		
 
 		#condition
-		if appointment_type == u'Odontológica':
+		if tipo_historia == u'doctor_dental_care':
 			result = data_obj._get_id(cr, uid, 'doctor_dental_care', 'view_doctor_hc_odonto_form')
 			view_id = data_obj.browse(cr, uid, result).res_id
 			context['default_patient_id'] = context.get('patient_id')
@@ -71,7 +71,7 @@ class doctor_appointment(osv.osv):
 
 		elif self.pool.get('doctor.doctor').modulo_instalado(cr, uid, 'doctor_biological_risk', context=context):	
 
-			if appointment_type == u'Riesgo Biologico':
+			if tipo_historia == u'doctor_biological_risk':
 				result = data_obj._get_id(cr, uid, 'doctor_biological_risk', 'doctor_atencion_ries_bio_form_view')
 				view_id = data_obj.browse(cr, uid, result).res_id
 				context['default_patient_id'] = context.get('patient_id')
@@ -91,7 +91,7 @@ class doctor_appointment(osv.osv):
 
 		elif self.pool.get('doctor.doctor').modulo_instalado(cr, uid, 'doctor_psychology', context=context):
 
-			if appointment_type.lower().find('psicologia') != -1 or appointment_type.lower().find(u'psicología') != -1:
+			if tipo_historia == "doctor_psychology":
 				result = data_obj._get_id(cr, uid, 'doctor_psychology', 'doctor_psicologia_form_view')
 				view_id = data_obj.browse(cr, uid, result).res_id
 				context['default_patient_id'] = context.get('patient_id')
